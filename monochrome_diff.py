ace5837afea35a149b6cd7eab770643038228779
Anwesh Saha <anweshsaha21@iitk.ac.in>
2024-03-28 17:05:36 +0530

    combined plot

--- monochrome_diff.py
+++ monochrome_diff.py
@@ -19,17 +19,24 @@ x_values = np.arange(-10, 50, 0.2)
 cos_integral_values = np.array([integral_function(cos_integrand, x) for x in x_values])
 sin_integral_values = np.array([integral_function(sin_integrand, x) for x in x_values])
 
-result_values = (cos_integral_values + sin_integral_values)
-result_values[x_values < 0] = 0
+monochromatic_intensity = (cos_integral_values + sin_integral_values)
+monochromatic_intensity[x_values < 0] = 0
 
-plt.plot(x_values, result_values)
+plt.plot(x_values, monochromatic_intensity)
 plt.title('500nm')
 plt.xlabel('x (m)')
 plt.ylabel('Intensity')
 plt.grid(True)
-folder_path = "plot"
-file_name = f"plot_{lmda*1e9:.0f}nm.png" 
+folder_path = "plots"
+file_name = f"plot {lmda*1e9:.0f}nm.png" 
 
 file_path = os.path.join(folder_path, file_name)
 plt.savefig(file_path)
+
+folder_path = "int_values"
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+file_path = os.path.join(folder_path, f"monochromatic {lmda*1e9:.0f}nm")
+np.save(file_path, monochromatic_intensity)
+
 plt.show()
--- polychrome_diff.py
+++ polychrome_diff.py
@@ -5,9 +5,6 @@ from scipy.integrate import quad
 import os
 from params import *
 
-lmda_min = lmda - BW 
-lmda_max = lmda + BW   
-
 def cos_integrand(t):
     return np.cos(np.pi * t **2 / (dist * lmda))
 
@@ -21,13 +18,11 @@ def integral_function(integrand_func, x):
 x_values = np.arange(-10, 50, 0.2)
 polychromatic_intensity = np.zeros_like(x_values)
 
-for lmda in np.arange(lmda_min, lmda_max, 5e9):
+for i in np.arange(lmda_min, lmda_max, alpha):
     cos_integral_values = np.array([integral_function(cos_integrand, x) for x in x_values])
     sin_integral_values = np.array([integral_function(sin_integrand, x) for x in x_values])
-
-    result_values = cos_integral_values + sin_integral_values
-    result_values[x_values < 0] = 0  # Set intensity before knife edge to zero
-
+    result_values = 0.1*(cos_integral_values + sin_integral_values)
+    result_values[x_values < 0] = 0  
     polychromatic_intensity  += result_values
 
 plt.plot(x_values, polychromatic_intensity )
@@ -36,11 +31,20 @@ plt.xlabel('x (m)')
 plt.ylabel('Intensity')
 plt.grid(True)
 
-folder_path = "poly_plot"
+folder_path = "plots"
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
-file_name = f"plot {(lmda-BW)*1e9:.0f} - {(lmda+BW)*1e9:.0f}nm.png"
+file_name = f"plot {(lmda_min)*1e9:.0f} - {(lmda_max)*1e9:.0f}nm.png"
 file_path = os.path.join(folder_path, file_name)
 plt.savefig(file_path)
+
+folder_path = "int_values"
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+file_path = os.path.join(folder_path, f"polychromatic {(lmda_min)*1e9:.0f} - {(lmda_max)*1e9:.0f}nm")
+np.save(file_path, polychromatic_intensity)
+
 plt.show()
