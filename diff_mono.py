e2f8e2bf99a815c3f3388fc49fbbe3b8f3e01f3e
Anwesh Saha <anweshsaha21@iitk.ac.in>
2024-03-30 19:24:44 +0530

    added requirements.txt

--- diff_mono.py
+++ diff_mono.py
@@ -1,15 +1,22 @@
 import numpy as np
 from scipy.special import fresnel
+from scipy.signal import convolve
 import matplotlib.pyplot as plt
 import os
 from params import *
+radius =np.sqrt(2 / (lmda * dist)) * 19/2
 
 u_values = np.sqrt(2 / (lmda * dist)) * np.linspace(-50, 10, 1000)
 S, C = fresnel(u_values)
-
 intensity = 0.5 * ((0.5 - C)**2 + (0.5 - S)**2)
 
+# x_values = np.linspace(-radius, radius, len(u_values))
+# kernel = 2 * np.sqrt(radius**2 - (x_values)**2)
+# convolved_intensity = convolve(intensity, kernel, mode='same')
+
+plt.figure(figsize=(10, 5))
 plt.plot(u_values, intensity, label="Fresnel Intensity")
+# plt.plot(u_values, kernel, label="Convolved Intensity")
 plt.xlabel("Fresnel Number (u)")
 plt.ylabel("Normalized Intensity")
 plt.legend()
--- params.py
+++ params.py
@@ -1,4 +1,4 @@
-lmda = 400e-9 #wavelength in m
+lmda = 500e-9 #wavelength in m
 BW = 400e-9
 dist = 3.84e8 #distance of the screen from the slit
 lmda_min = lmda
